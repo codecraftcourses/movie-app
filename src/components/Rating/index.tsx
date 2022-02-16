b15eaffc9239f51487d567cfcec1217c352f4b91
codecraftcourses <codecraftcourses@outlook.com>
2022-02-16 15:06:39 +0000

    fix: remove focus outline

--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -5,7 +5,7 @@ import Badge from '../Badge';
 function Rating({ movie }: RatingProps) {
   return (
     <Badge color="primary" icon="star">
-      {movie.vote_average}
+      {movie.vote_average.toFixed(1)}
     </Badge>
   );
 }
