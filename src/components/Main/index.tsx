bf4fe02d6e206441b7e29d51a006f8b5bd6a2f14
codecraftcourses <codecraftcourses@outlook.com>
2022-02-16 19:54:58 +0000

    feat: decrease padding on mobile

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import type MainProps from './MainProps';
 
 function Main({ children }: MainProps) {
-  return <main className="container space-y-4 py-8">{children}</main>;
+  return <main className="container space-y-4 py-4 md:py-8">{children}</main>;
 }
 
 export default Main;
